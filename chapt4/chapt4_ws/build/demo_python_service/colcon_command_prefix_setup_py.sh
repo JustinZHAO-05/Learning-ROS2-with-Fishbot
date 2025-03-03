0c54415a9adf38ff9a04760927fa26dcd01ecc54
JustinZHAO-05 <justinzhao050322@gmail.com>
2025-03-03 22:21:13 +0800

    face recognition

---
+++ chapt4/chapt4_ws/build/demo_python_service/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/fishros/document/GitHub/Learning-ROS2-with-Fishbot/chapt4/chapt4_ws/install/chapt4_interfaces/share/chapt4_interfaces/package.sh"
